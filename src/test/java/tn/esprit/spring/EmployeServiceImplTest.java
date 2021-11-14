cf52d041054301bc9693ebbf2158c2fecb7dc332
archene <archene9@gmail.com>
2021-11-14 23:05:43 +0100

    employee

--- src/test/java/tn/esprit/spring/EmployeServiceImplTest.java
+++ src/test/java/tn/esprit/spring/EmployeServiceImplTest.java
@@ -1,114 +1,76 @@
 package tn.esprit.spring;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
-
-import java.util.List;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
 
-import tn.esprit.spring.entities.Employe;
-import tn.esprit.spring.entities.Role;
+import tn.esprit.spring.entities.*;
+import tn.esprit.spring.repository.ContratRepository;
 import tn.esprit.spring.repository.EmployeRepository;
 import tn.esprit.spring.services.EmployeServiceImpl;
+
+import java.util.Date;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import tn.esprit.spring.services.EntrepriseServiceImpl;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
-public class EmployeServiceImplTest {
-
-	
-	@Autowired
-	EmployeServiceImpl esp; 
-	
-	@Autowired
-	EmployeRepository  emprepo; 
-	
-	
-	private static final Logger l = LogManager.getLogger(EmployeServiceImplTest.class);
-	
-	@Test
-	 public void contextLoads() {
-		
-	}
-	
-	/*
-	@Test
-	public void testCreateEmploye() {
-		 
-	
-		 int a = esp.ajouterEmploye(new Employe("ben letaief", "archene", "archene9@gmail.com", true, Role.ADMINISTRATEUR));
-		 assertThat(a).isGreaterThan(0);
-			l.info("idEmplo: " + a);
-	
-	}*/
+class EmployeTest {
+    @Autowired
+    private EmployeRepository employeRepository;
+    @Autowired
+    private EmployeServiceImpl employeService;
+    @Autowired
+    private ContratRepository contratRepository;
+    @Autowired
+    EntrepriseServiceImpl entrepriseService;
+    private final static Logger l = LogManager.getLogger(EmployeTest.class);
 /*
-	@Test
-	public void verifEmailEmploye() {
-		Employe emp = esp.getAllEmployes().get(0);
-		System.out.println("****************************");
-		assertEquals( "archene15@gmail.com",emp.getEmail());
-		assertTrue(emp.getEmail().contains("@"));
-		l.info("employe Email: " + emp.getEmail());
-		
-	}
-	*/
-	/*
-	@Test
-	public void getNombreEmploye() {
-		int emps = esp.getNombreEmployeJPQL();
-		System.out.println("****************************");
-		assertTrue(emps > 0);
-		l.info("NombreEmploye: " + emps);
-	}
-	*/
-	
-	@Test
-	public void getAllEmployes() {
-		List<Employe> emps = esp.getAllEmployes();
-		System.out.println("****************************");
-		assertTrue(emps.size() > 0);
-		
-		l.info("les employes: " + emps);
-	}
-	/*
-	@Test
-	public void getAllEmployeNames() {
-		List<String> emps = esp.getAllEmployeNamesJPQL();
-		System.out.println("****************************");
-		assertTrue(emps.size() > 0);
-		l.info("Taille: " + emps);
-	}*/
-	
-	/*
-	@Test
-	public void modifier_emp() {
-		 esp.mettreAjourEmailByEmployeId("archene9@gmail.com", 1);
-		System.out.println("****************************");
-		assertEquals("archene9@gmail.com",esp.getAllEmployes().get(0).getEmail());
-		l.info("Email: " + esp.getAllEmployes().get(0).getEmail());
-	}
+    @Test
+    void ajoutEmploye() {
+        Employe employe = new Employe("taherarchene", "benletaief", "benletaief.taherarchene@esprit.tn", true, Role.CHEF_DEPARTEMENT);
+        employeService.ajouterEmploye(employe);
+        long start = System.currentTimeMillis();
+        long elapsedTime = System.currentTimeMillis() - start;
+        l.info("Method execution time: " + elapsedTime + " milliseconds.");
+        l.info("l'employé est ajouté");
+         employeService.deleteEmployeById(employe.getId());
+    }
 */
-	/*
-	@Test
-	public void supp_emp() {
-		 esp.deleteEmployeById(2);
-		System.out.println("****************************");
-		assertEquals("archene9@gmail.com",esp.getAllEmployes().get(0).getEmail());
-		l.info("nom: " + esp.getAllEmployes().get(0).getNom());
-	}*/
-	}
+    
+    @Test
+    void getNombreEmploye() {
+        int nombre = employeService.getNombreEmployeJPQL();
+        Employe employe = new Employe("taherarchene", "benletaief", "benletaief.taherarchene@esprit.tn", true, Role.CHEF_DEPARTEMENT);
+        employeService.ajouterEmploye(employe);
+        int secondNombre = employeService.getNombreEmployeJPQL();
+        Assertions.assertNotEquals(nombre, secondNombre);
+        l.info("le nombre des employés est : " + nombre);
+        employeService.deleteEmployeById(employe.getId());
+    }
+
+
+
+  
+  
+
+    @Test
+    void DeleteEmploye() {
+    	Employe employe = new Employe("taherarchene", "benletaief", "benletaief.taherarchene@esprit.tn", true, Role.CHEF_DEPARTEMENT);
+        employeService.ajouterEmploye(employe);
+        boolean existBeforeDelete = employeRepository.findById(employe.getId()).isPresent();
+        Assert.assertTrue(existBeforeDelete);
+        employeService.deleteEmployeById(employe.getId());
+        boolean existAfterDelete = employeRepository.findById(employe.getId()).isPresent();
+        Assert.assertFalse(existAfterDelete);
+        l.info("L'employé est supprimé");
+    }
+
+  
+}
